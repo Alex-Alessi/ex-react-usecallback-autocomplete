f717f846fd8c8ef021bd249209657ab0f18a2173
Alex-Alessi <alex.alessi246@gmail.com>
2025-04-09 17:07:20 +0200

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [query, setQuery] = useState("");
-  const fetchRequest = (query) => {
-    fetch(
-      `https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`
-    )
-      .then((res) => res.json())
-      .then((data) => d);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    if (query.length !== 0) {
+      fetch(
+        `https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`
+      )
+        .then((res) => res.json())
+        .then((data) => setResults(data))
+        .catch((error) => console.error(error));
+    } else {
+      setResults([]);
+    }
+  }, [query]);
+
+  console.log(results);
+
+  const Card = ({ image, name, description }) => {
+    return (
+      <div className="card">
+        <img src={image}></img>
+        <p>
+          <b>Name:</b> {name}
+        </p>
+        <b>Description:</b> {description}
+      </div>
+    );
   };
+
   return (
     <div>
       <input
@@ -17,9 +39,16 @@ function App() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       ></input>
-      <div className="results-container">
-        <h1>Suggerimenti</h1>
-      </div>
+      {query.length > 0 && (
+        <div className="tendina">
+          <h1>Suggerimenti</h1>
+          <div className="results-style">
+            {results.map((result) => (
+              <Card key={result.id} {...result} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
