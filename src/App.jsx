21fa893e313173f847824557047bbf39840f9b2d
Alex-Alessi <alex.alessi246@gmail.com>
2025-04-10 12:46:16 +0200

    debounce con useCallback e errore fixato

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./App.css";
 
 function debounce(callback, delay) {
@@ -15,14 +15,24 @@ function App() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
 
-  const fetchexecute = debounce((query) => {
-    fetch(
-      `https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`
-    )
-      .then((res) => res.json())
-      .then((data) => setResults(data))
-      .catch((error) => console.error(error));
-  }, 300);
+  const fetchexecute = useCallback(
+    debounce((query) => {
+      console.log("🔄 Fetch chiamato per:", query);
+      fetch(
+        `https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (Array.isArray(data)) {
+            setResults(data);
+          } else {
+            setResults([]);
+          }
+        })
+        .catch((error) => console.error(error));
+    }, 2000),
+    []
+  );
 
   useEffect(() => {
     if (query.length !== 0) {
@@ -55,12 +65,3 @@ function App() {
 }
 
 export default App;
-
-// Milestone 2: Implementare il Debounce per Ottimizzare la Ricerca
-// Attualmente, ogni pressione di tasto esegue una richiesta API. Questo è inefficiente!
-// Implementa una funzione di debounce per ritardare la chiamata API fino a quando l’utente
-// smette di digitare per un breve periodo (es. 300ms)
-// Dopo l’implementazione, verifica che la ricerca non venga eseguita immediatamente a ogni
-// tasto premuto, ma solo dopo una breve pausa.
-
-// Obiettivo: Ridurre il numero di richieste API e migliorare le prestazioni.
